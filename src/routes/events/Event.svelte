82d43a99f7d8625f1ed39b77911e2db33c2d6022
execut4ble <jokubas.banaitis@gmail.com>
2024-11-22 14:01:11 +0200

    Event component layout fixes

--- src/routes/events/Event.svelte
+++ src/routes/events/Event.svelte
@@ -8,55 +8,57 @@
 
   const date: Date = new Date(event.date);
 
-  const year: Number = date.getFullYear();
+  const year: Number = date.getUTCFullYear();
   const month: String = date.toLocaleString("en-us", { month: "short" });
-  const day: Number = date.getDay();
+  const day: Number = date.getUTCDate();
 </script>
 
 <div class="event">
-  <div class="eventHeading">
-    <div class="date">
-      <div class="month">
-        {month}
-      </div>
-      <div class="day">
-        <strong>{day}</strong>
-      </div>
-      <div class="year">
-        {year}
-      </div>
+  <div class="date">
+    <div class="month">
+      {month}
     </div>
+    <div class="day">
+      <strong>{day}</strong>
+    </div>
+    <div class="year">
+      {year}
+    </div>
+  </div>
+  <div class="eventInfo">
     <div class="title">
       <h2><a href="/events/{event.slug}"><strong>{event.title}</strong></a></h2>
       <p>{date.toUTCString()}</p>
     </div>
-  </div>
-  <div class="eventBody">
-    {event.description}
+    <div class="eventBody">
+      {event.description}
+    </div>
   </div>
 </div>
 
 <style>
-  div.eventHeading {
+  div.event {
     display: flex;
     flex-direction: row;
-    gap: 2em;
+    gap: 1em;
+    margin-bottom: 2em;
   }
 
-  div.eventHeading .date {
+  div.event .date {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: start;
     align-items: center;
     gap: 0.5em;
     max-width: 2.5em;
+    margin: 1em;
   }
 
-  div.eventHeading .date .day {
+  div.event .date .day {
     font-size: 20pt;
   }
 
-  .event .eventHeading .date .month {
+  div.event .date .month {
     text-transform: uppercase;
   }
 
