7309154d51690b19142348acb4b97f2fa1011b7a
execut4ble <jokubas.banaitis@gmail.com>
2024-11-22 15:08:37 +0200

    Use a database connection pool for queries

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
-import postgres from 'postgres';
-import 'dotenv/config'
+import { sql } from '$lib/db';
 
-export const handle: Handle = async ({event, resolve}) => {
-  const sql = postgres('postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME);
-
-  event.locals = {
-    sql: sql
-  };
-  
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.sql = sql;
   const response = await resolve(event);
   return response;
 };
