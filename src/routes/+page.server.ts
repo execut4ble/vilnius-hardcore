908ffc2900b8ad822ca046372caeb1a960bbaa33
execut4ble <jokubas.banaitis@gmail.com>
2024-12-02 23:45:00 +0200

    Order homepage events by date ascending

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ locals }) => {
     const { sql } = locals;
 
-    const events = await sql`select * from events where date >= CURRENT_DATE order by date desc`
+    const events = await sql`select * from events where date >= CURRENT_DATE order by date asc`
 
     return {events};
 }) satisfies PageServerLoad;
