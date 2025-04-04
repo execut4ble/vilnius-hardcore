1ed15b804d53914c85b133da0b234f72a1852ec3
execut4ble <jokubas.banaitis@gmail.com>
2025-04-04 13:17:28 +0300

    Fix invalid align attribute and formatting in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,20 +6,75 @@
 <h1>Lithuania underground scene</h1>
 
 <ul>
-	<li><a href="https://mp3.hardcore.lt">Music archve: since first tape-recorded punk till now</a></li>
+  <li>
+    <a href="https://mp3.hardcore.lt"
+      >Music archve: since first tape-recorded punk till now</a
+    >
+  </li>
 </ul>
 
 <h2>Vilnius</h2>
-<p>"The" DIY underground venue is <strong><a href="https://oldschool.hardcore.lt/xi20/xi20.html">XI20</a></strong>. From punk to vanguard jazz, hardcore to noise.</p>
-<p>If you want to play at XI20 with your band please contact: <strong>1120 (A) hardcore.lt</strong></p>
-<p>Travellers are also welcome: as locals how to find the place or if you are desperate as for directions at <strong>tourist (A) hardcore.lt</strong></p>
-<p>For cleaner artsier vibes check out <a href="https://instagram.com/retrito.smarsas">RETRITO SMARSAS</a></p>
-<p><a href="https://kirtimukc.lt">KKC</a> aka Kirtimų Kultūros Centras is a city-run cultural center staffed by enthusiastic people. Get in touch with them if you're noise or folk act: <strong>renginiai (A) kirtimukc.lt</p>
-<p><a href="https://www.facebook.com/pg/facialtumourbooking/about/">Facial Tumour Booking</a> -- booking crew for dark underground music</p>
+<p>
+  "The" DIY underground venue is <strong
+    ><a href="https://oldschool.hardcore.lt/xi20/xi20.html">XI20</a></strong
+  >. From punk to vanguard jazz, hardcore to noise.
+</p>
+<p>
+  If you want to play at XI20 with your band please contact: <strong
+    >1120 (A) hardcore.lt</strong
+  >
+</p>
+<p>
+  Travellers are also welcome: as locals how to find the place or if you are
+  desperate as for directions at <strong>tourist (A) hardcore.lt</strong>
+</p>
+<p>
+  For cleaner artsier vibes check out <a
+    href="https://instagram.com/retrito.smarsas">RETRITO SMARSAS</a
+  >
+</p>
+<p>
+  <a href="https://kirtimukc.lt">KKC</a> aka Kirtimų Kultūros Centras is a
+  city-run cultural center staffed by enthusiastic people. Get in touch with
+  them if you're noise or folk act: <strong>renginiai (A) kirtimukc.lt</strong>
+</p>
+<p>
+  <a href="https://www.facebook.com/pg/facialtumourbooking/about/"
+    >Facial Tumour Booking</a
+  > -- booking crew for dark underground music
+</p>
 <h2>Kaunas</h2>
-<p>Metalhead/"apolitical" venue <a href="https://www.facebook.com/LemmyKlubasKaunas/">Lemmy</a></p>
+<p>
+  Metalhead/"apolitical" venue <a
+    href="https://www.facebook.com/LemmyKlubasKaunas/">Lemmy</a
+  >
+</p>
 <h2>Alytus</h2>
-<p>There is a <strong><a href="http://www.facebook.com/groups/259810918716/" target="_blank" rel="noopener">Fenix bar</a></strong></p>
+<p>
+  There is a <strong
+    ><a
+      href="http://www.facebook.com/groups/259810918716/"
+      target="_blank"
+      rel="noopener">Fenix bar</a
+    ></strong
+  >
+</p>
 <h2>World</h2>
-<p>There’s a guide to underground venues/promoters/etc in Eastern Europe, made by the noise-guru Arma: <a href="http://www.arma.lt/p/geography-of-sound.html">http://www.arma.lt/p/geography-of-sound.html</a></p>
-<p align="right"><em>last update: November 2020</em></p>
+<p>
+  There’s a guide to underground venues/promoters/etc in Eastern Europe, made by
+  the noise-guru Arma: <a href="http://www.arma.lt/p/geography-of-sound.html"
+    >http://www.arma.lt/p/geography-of-sound.html</a
+  >
+</p>
+<p class="align-right"><em>last update: November 2020</em></p>
+
+<style>
+  p.align-right {
+    text-align: right;
+  }
+
+  h2 {
+    margin-top: 1em;
+    margin-bottom: 0;
+  }
+</style>
