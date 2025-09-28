6ca1273069602966f72c8265da626e90aaa7d265
execut4ble <jokubas.banaitis@gmail.com>
2025-09-28 22:07:58 +0300

    Revert svelte config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const config: import("@sveltejs/kit").Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: [vitePreprocess()],
