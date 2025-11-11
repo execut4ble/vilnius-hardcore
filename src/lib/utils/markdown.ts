98a7720f3197c0c6904ca80d835dd1a80e1d7a6e
execut4ble <jokubas.banaitis@gmail.com>
2025-11-11 19:07:53 +0200

    Fix markdown to plaintext conversion

--- src/lib/utils/markdown.ts
+++ src/lib/utils/markdown.ts
@@ -1,7 +1,9 @@
 import removeMd from "remove-markdown";
 
 const toText = (markdown: string) => {
-  return removeMd(markdown);
+  const blocks = markdown.split(/\n{2,}/);
+  const cleaned = blocks.map((b) => removeMd(b)).join("\n\n");
+  return cleaned;
 };
 
 export { toText as markdownToText };
